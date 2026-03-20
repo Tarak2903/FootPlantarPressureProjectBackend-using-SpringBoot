0ef92d4d916d2ffcfe6e687cbe6828cd70d43141
Tarak2903 <tarakkhurana29@gmail.com>
2026-03-20 21:01:48 +0530

    1.Role based add in auth

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                                         "/swagger-ui.html",
                                         "/actuator/*"
                                 ).permitAll()
+                                .requestMatchers("/admin/*").hasRole("ADMIN")
                                 .anyRequest().
                                 authenticated()
 
